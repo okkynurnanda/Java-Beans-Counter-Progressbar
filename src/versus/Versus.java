fe1077ecd72dae4339db0789b4ffe0f257fc1f12
okkynurnanda <38342923+okkynurnanda@users.noreply.github.com>
2020-05-16 05:32:18 +0700

    Add files via upload

---
+++ src/versus/Versus.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package versus;
+
+/**
+ *
+ * @author okky
+ */
+public class Versus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //ganti versus1/versus2
+        versus2 b  = new versus2();
+        b.setVisible(true);
+    }
+    
+}
